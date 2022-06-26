228d66eee32916d1f141bd1fb520e4f2de554960
Denitsa Ilieva <Ilieva@stud.uni-heidelberg.de>
2022-06-26 11:15:26 +0200

    feat: add link widget into WebAppGenerator

--- src/components/internal/tools.js
+++ src/components/internal/tools.js
@@ -7,6 +7,8 @@ import Header from "@editorjs/header";
 import List from "@editorjs/list";
 import Alert from "editorjs-alert";
 import Table from "@editorjs/table";
+//import LinkTool from "@editorjs/link";
+//import RawTool from "@editorjs/raw";
 
 export const EDITOR_JS_TOOLS = {
   // NOTE: Paragraph is default tool. Declare only when you want to change paragraph option.
@@ -22,5 +24,7 @@ export const EDITOR_JS_TOOLS = {
       messagePlaceholder: "Enter something",
     },
   },
+  //raw: RawTool,
+  //linkTool: LinkTool,
   table: { class: Table, inlineToolbar: true, shortcut: "CMD+SHIFT+T" },
 };

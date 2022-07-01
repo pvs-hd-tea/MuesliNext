55dd156df5f3e8e83502da00eb5386aa74530e58
Ingmar <ingmar@lowack.com>
2022-07-01 18:11:37 +0200

    feat: copy code from alert widget

--- src/components/Widgets/Button.tsx
+++ src/components/Widgets/Button.tsx
@@ -1,23 +1,39 @@
 import "./Button.css";
-import React from "react";
+import React, { useState } from "react";
 import LocalDataService from "../../data/services/localDataService";
 import { createRoot } from "react-dom/client";
 
 interface ButtonData {
   text: string;
+  type: string; // TODO: make enum
 }
 
 export default class Button {
   data: ButtonData;
   wrapper?: HTMLElement;
   readOnly: boolean;
+  api: EditorJS.API;
+  container?: HTMLElement;
 
-  constructor({ data, readOnly }: { data: ButtonData; readOnly: boolean }) {
+  constructor({
+    data,
+    readOnly,
+    api,
+  }: {
+    data: ButtonData;
+    readOnly: boolean;
+    api: EditorJS.API;
+  }) {
     this.readOnly = readOnly;
     this.data = {
       text: data.text !== undefined ? data.text : "",
+      type: Button.buttonTypes.includes(data.type)
+        ? data.type
+        : Button.defaultType,
     };
     this.wrapper = undefined;
+    this.api = api;
+    this.container = undefined;
   }
 
   static get toolbox() {
@@ -29,6 +45,14 @@ export default class Button {
 
   static get isReadOnlySupported() {
     return true;
+  }
+
+  static get defaultType() {
+    return "link";
+  }
+
+  static get buttonTypes() {
+    return ["link", "alert"];
   }
 
   render() {
@@ -52,6 +76,117 @@ export default class Button {
 
     return this.wrapper;
   }
+
+  /**
+   * Alert Tool`s styles
+   *
+   * @returns {Object}
+   */
+  get CSS() {
+    return {
+      settingsButton: this.api.styles.settingsButton,
+      settingsButtonActive: this.api.styles.settingsButtonActive,
+      wrapper: "cdx-alert",
+      wrapperForType: (type: string) => `cdx-alert-${type}`,
+      message: "cdx-alert__message",
+    };
+  }
+
+  /**
+   * Create Block's settings block
+   *
+   * @returns {HTMLElement}
+   */
+  renderSettings() {
+    const settingsContainer = this._make("div");
+
+    Button.buttonTypes.forEach((type) => {
+      const settingsButton = this._make(
+        "div",
+        [
+          this.CSS.settingsButton,
+          this.CSS.wrapper,
+          this.CSS.wrapperForType(type),
+        ],
+        {
+          innerHTML: "B",
+        }
+      );
+
+      if (this.data.type === type) {
+        // Highlight current type button
+        settingsButton.classList.add(this.CSS.settingsButtonActive);
+      }
+
+      // Set up click handler
+      settingsButton.addEventListener("click", () => {
+        this._changeAlertType(type);
+
+        // Un-highlight previous type button
+        settingsContainer
+          .querySelectorAll(`.${this.CSS.settingsButton}`)
+          .forEach((button: any) =>
+            button.classList.remove(this.CSS.settingsButtonActive)
+          );
+
+        // and highlight the clicked type button
+        settingsButton.classList.add(this.CSS.settingsButtonActive);
+      });
+
+      settingsContainer.appendChild(settingsButton);
+    });
+
+    return settingsContainer;
+  }
+
+  /**
+   * Helper for changing style of Alert block with the selected Alert type
+   *
+   * @param {string} newType - new Alert type to be applied to the block
+   * @private
+   */
+  _changeAlertType(newType: string) {
+    // Save new type
+    this.data.type = newType;
+
+    Button.buttonTypes.forEach((type) => {
+      const alertClass = this.CSS.wrapperForType(type);
+
+      // Remove the old Alert type class
+      this.wrapper!.classList.remove(alertClass);
+
+      if (newType === type) {
+        // Add an Alert class for the selected Alert type
+        this.wrapper!.classList.add(alertClass);
+      }
+    });
+  }
+
+  /**
+   * Helper for making Elements with attributes
+   *
+   * @param  {string} tagName           - new Element tag name
+   * @param  {array|string} classNames  - list or name of CSS classname(s)
+   * @param  {Object} attributes        - any attributes
+   * @returns {Element}
+   * @private
+   */
+  _make(tagName: any, classNames: any = undefined, attributes: any = {}) {
+    const el = document.createElement(tagName);
+
+    if (Array.isArray(classNames)) {
+      el.classList.add(...classNames);
+    } else if (classNames) {
+      el.classList.add(classNames);
+    }
+
+    for (const attrName in attributes) {
+      el[attrName] = attributes[attrName];
+    }
+
+    return el;
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   save(blockContent: any) {
     if (!this.readOnly) {
@@ -59,6 +194,7 @@ export default class Button {
 
       return {
         text: textInput.value,
+        type: this.data.type,
       };
     }
     return this.data;
@@ -71,41 +207,49 @@ interface Props {
   readOnly: boolean;
 }
 
-class ButtonComponent extends React.Component<Props, ButtonData> {
-  state = { ...this.props.initData };
+const ButtonComponent: React.FC<Props> = ({
+  onDataChange,
+  initData,
+  readOnly,
+}) => {
+  const [data, setData] = useState(initData);
 
-  dataService = LocalDataService.getFromLocalOrNew();
-
-  render() {
-    if (this.props.readOnly) {
-      return (
-        <div className="dynamic-value-component-display">
-          <button className="text-white bg-green-500 hover:bg-green-400 shadow-sm hover:shadow-md ml-1 p-1 pl-3 pr-3 rounded-lg">
-            {this.state.text == "" ? "Button" : this.state.text}
-          </button>
-        </div>
-      );
-    } else {
-      return (
-        <div className="dynamic-value-component-configure">
-          <input
-            id="textInput"
-            className="text-input"
-            type="text"
-            value={this.state.text}
-            onChange={(event) => {
-              this.setState(
-                Object.assign(this.state, { text: event.target.value })
-              );
-              this.props.onDataChange(this.state);
-            }}
-            placeholder="Enter text name..."
-          />
-          <button className="text-white bg-green-500 hover:bg-green-400 shadow-sm hover:shadow-md ml-1 p-1 pl-3 pr-3 rounded-lg">
-            {this.state.text == "" ? "Button" : this.state.text}
-          </button>
-        </div>
-      );
-    }
+  let btnColor = "";
+  if (data.type === "link") {
+    btnColor = "bg-blue-500 hover:bg-blue-400";
+  } else if (data.type === "alert") {
+    btnColor = "bg-red-500 hover:bg-red-400";
   }
-}
+
+  if (readOnly) {
+    return (
+      <div className="dynamic-value-component-display">
+        <button
+          className={`text-white ${btnColor} shadow-sm hover:shadow-md ml-1 p-1 pl-3 pr-3 rounded-lg`}
+        >
+          {data.text == "" ? "Button" : data.text}
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="dynamic-value-component-configure">
+        <input
+          id="textInput"
+          className="text-input"
+          type="text"
+          value={data.text}
+          onChange={(event) => {
+            setData({ ...data, text: event.target.value });
+          }}
+          placeholder="Enter text name..."
+        />
+        <button
+          className={`text-white ${btnColor} shadow-sm hover:shadow-md ml-1 p-1 pl-3 pr-3 rounded-lg`}
+        >
+          {data.text == "" ? "Button" : data.text}
+        </button>
+      </div>
+    );
+  }
+};
